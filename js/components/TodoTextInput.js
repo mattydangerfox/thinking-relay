cbe4b6b26db43a753b330121df2a92d5caa1b12f
Sanghee Kim <sangheestyle@gmail.com>
2017-06-10 12:13:05 -0400

    focus the text input right after loding the page
    User expects the text input should be focused on the page right
    after locing the page.
    
    issue #2

--- js/components/TodoTextInput.js
+++ js/components/TodoTextInput.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 
 export default class TodoTextInput extends React.Component {
+  componentDidMount() {
+    ReactDOM.findDOMNode(this).focus();
+  }
+
   render() {
     return (
       <input
